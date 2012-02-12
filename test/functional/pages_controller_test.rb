3330e51bce808a61e711cb35db3724a5950a8c1f
William Czaja <william.czaja@gmail.com>
2012-02-12 05:35:16 -0800

    Added pages

---
+++ test/functional/pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionController::TestCase
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "should get projects" do
+    get :projects
+    assert_response :success
+  end
+
+end
