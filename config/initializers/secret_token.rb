0202ac4cc6fe0bd0f05eeb5bcff30682d7af9d27
William Czaja <william.czaja@gmail.com>
2012-02-12 05:12:20 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Czajavitae::Application.config.secret_token = '3af2251ae9143cd8111d3baf497986f324f927f78821c0b0c9958dc2f3031b1df8ff27f4a01ed772787f134a98e84ec7862b83277f21ddbd8afba0582ae5690d'
